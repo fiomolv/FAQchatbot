d04dafa557206e16c360ff6b8835056018a6b1bc
Xu Ruolan <XURUOLAN@SPGROUP.COM.SG>
2018-06-06 10:58:46 +0800

    add dialogflow recognizer and test script

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 'use strict';
 // Reference the packages we require so that we can use them in creating the bot
 const builder = require('botbuilder');
-const express = require('express')
 const restify = require('restify');
+const apiairecognizer = require('./apiairecognizer');
 
 // Config variables set in Heroku
-const MICROSOFT_APP_ID = "80d11cf5-4542-472b-a092-f170d139beb6" || process.env.MICROSOFT_APP_ID;
-const MICROSOFT_APP_PASSWORD = "piRRBSN395}@gcwhaTC66:~" || process.env.process.env.MICROSOFT_APP_PASSWORD;
+const MICROSOFT_APP_ID = "80d11cf5-4542-472b-a092-f170d139beb6"||process.env.MICROSOFT_APP_ID;
+const MICROSOFT_APP_PASSWORD = "piRRBSN395}@gcwhaTC66:~"|| process.env.MICROSOFT_APP_PASSWORD;
+const DIALOGFLOW_TOKEN = "cfc321ce45724d9e8d52c48c28b70cf6"|| process.env.DIALOGFLOW_TOKEN;
+
+const recognizer = new apiairecognizer(DIALOGFLOW_TOKEN);
+
 
 // =========================================================
 // Bot Setup
 // =========================================================
-// Listen for any activity on port 5000 of our local server
+// Listen for any activity on port 3978 of our local server
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -28,15 +32,23 @@ var connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 
-// If a Post request is made to /api/messages on port 5000 of our local server, then we pass it to the bot connector to handle
-/* microsoft bot framework endpoint */
+// If a Post request is made to /api/messages on port 3978 of our local server, then we pass it to the bot connector to handle
 server.post('/api/messages', connector.listen());
 
 // =========================================================
 // Bots Dialogs
 // =========================================================
 // This is called the root dialog. It is the first point of entry for any message the bot receives
-bot.dialog('/', function (session) {
-// Send 'hello world' to the user
-    session.send("Hello World");
+var intents = new builder.IntentDialog({
+    recognizers: [recognizer]
+});
+
+bot.dialog('/', intents);
+
+intents.matches('Default Welcome Intent', function(session, args) {
+    let messages = args.entities[0].response.messages;
+
+    messages.forEach((message) => {
+        session.send(message.speech);
+    })
 });
---
+++ test.js
@@ -0,0 +1,21 @@
+var builder = require('botbuilder');
+var apiairecognizer = require('./apiairecognizer');
+const DIALOGFLOW_TOKEN = process.env.DIALOGFLOW_TOKEN;
+
+var connector = new builder.ConsoleConnector().listen();
+
+var bot = new builder.UniversalBot(connector);
+
+var recognizer = new apiairecognizer(DIALOGFLOW_TOKEN);
+var intents = new builder.IntentDialog({
+    recognizers: [recognizer]
+});
+
+bot.dialog('/', intents);
+intents.matches('Default Welcome Intent', function(session, args) {
+    let messages = args.entities[0].response.messages;
+
+    messages.forEach((message) => {
+        session.send(message.speech);
+    })
+});
